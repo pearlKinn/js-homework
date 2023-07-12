04f8422cebd0db33092c77e45a22e8f7ff1c65da
pearlKinn <jinju000202@naver.com>
2023-07-13 00:45:41 +0900

    아이디 비밀번호 일치시 페이지 이동 구현

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -2,6 +2,7 @@ const emailInput = document.querySelector('.user-email-input');
 const passwordInput = document.querySelector('.user-password-input');
 const errorMessages = document.querySelectorAll('.error-message');
 const loginButton = document.querySelector('.btn-login');
+const form = document.querySelector('form');
 
 const user = {
   id:'asd@naver.com',
@@ -33,36 +34,23 @@ passwordInput.addEventListener('input', () => {
   validateInput(passwordInput, pwReg);
 });
 
+
 function checkUser(checkId, checkPw) {
   if ((checkId.value === user.id) && (checkPw.value === user.pw)) {
-    console.log('반갑습니다')
-    window.location.href = 'http://127.0.0.1:5500/mission01/naver_login/welcome.html';
-  }else {
+    window.location.href = 'welcome.html';
+  }else if(checkId.value.length === 0 || checkPw.value.length === 0) {
+    throw new Error('아이디와 비밀번호는 필수 입력값입니다.. ')
+  }
+  else {
     throw new Error('찾을 수 없는 회원입니다')
   }
 }
 
-loginButton.addEventListener('click', () => {
-  console.log(emailInput)
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   checkUser(emailInput, passwordInput);
 });
-
-
-// loginButton.addEventListener('click', () => {
-//   const emailInput = document.getElementById('emailInput').value;
-//   const passwordInput = document.getElementById('passwordInput').value;
-
-//   // 여기에 로그인 처리 로직을 작성합니다.
-//   // 예를 들어, 서버로 요청을 보내고 응답을 확인하여 로그인 성공 여부를 판단할 수 있습니다.
-
-//   // 로그인 성공 시
-//   if (로그인 성공 여부) {
-//     window.location.href = 'welcome.html';
-//   } else {
-//     // 로그인 실패 시 처리
-//     console.log('로그인 실패');
-//   }
-// });
 
 function emailReg(text){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
